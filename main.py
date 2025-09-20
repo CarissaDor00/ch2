042e987728f74b81e45d71fa02f97467b376d110
Carissa_Dorval <carissa.d.atc@gmail.com>
2025-09-19 21:23:00 -0500

    created a function to greet the user

--- main.py
+++ main.py
@@ -7,5 +7,15 @@ N/A
 Returns 
 None
 """
+def greeting(user):
+    """
+    Greets the user with Hello World
+    Args:
+        user: string 
+    
+    Returns
+        N/A
+    """
+    print(f"{user}, Hello World!")
 
-print("Hello World!")
+greeting("Obama") 
